0944fcbb095e33f2473d7e9c6b8691466d38db41
pagarciaSim <pablo.garciasimavilla@gmail.com>
2026-08-24 19:39:56 +0200

    first commit

---
+++ ai-research-agent/src/test/java/com/pgs/ai_research_agent/AiResearchAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgs.ai_research_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResearchAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
